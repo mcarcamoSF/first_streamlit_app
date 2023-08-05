f9cda5836db8b09b7de991fcd4229b457b93cc13
mcarcamoSF <140769523+mcarcamoSF@users.noreply.github.com>
2023-08-05 14:52:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
+pip install snowflake-connector-python
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
